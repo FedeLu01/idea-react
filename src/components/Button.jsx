9d30a685647fd4fbfbf282a36c2a7877407c6d6d
FedeLu01 <fedelucero95@gmail.com>
2021-08-01 23:06:20 -0300

    agregado de NavBar (funcionando)

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Button({name}) {
+
+    return(
+        <>
+            <button>Hola {name}</button>
+        </>
+    )
+}
+
+export default Button;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <>
+            <button>hola fede</button>
+        </>
+    )
+}
+
+export default NavBar;
